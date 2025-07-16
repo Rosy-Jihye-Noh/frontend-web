ad727e0fb4bcc16d7cdd70e88d1e86eb2c6e2320
InwooJeong <sinth92@hotmail.com>
2025-07-16 09:40:15 +0900

    config(server_config): [update and adjust server configuration files]

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'http://192.168.2.6:8081/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
